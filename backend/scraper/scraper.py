8ed6807174155d2f532e5a95edb768eac6e74767
Hamza Noureddine <hamzand02@gmail.com>
2025-11-18 19:05:14 +0200

    Adding scraper functionalities

--- backend/scraper/scraper.py
+++ backend/scraper/scraper.py
@@ -1,39 +1,32 @@
+from bs4 import BeautifulSoup
 import requests
 
 
-end=30
-url = f"https://www.mtv.com.lb/en/api/articles?start=0&end={end}&type="
 
-
-headers = {
-  'sec-ch-ua-platform': '"Windows"',
+def fetch_article_list(limit=50):
+   url = f"https://www.mtv.com.lb/en/api/articles?start=0&end={limit}&type="
+   headers = {'sec-ch-ua-platform': '"Windows"',
   'Referer': 'https://www.mtv.com.lb/en',
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
   'Accept': 'application/json, text/plain, */*',
   'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
-  'sec-ch-ua-mobile': '?0'
-}
-
-response = requests.request("GET", url, headers=headers)
-
-
-data = response.json()
-
-articles = data
-
-for article in articles:
+  'sec-ch-ua-mobile': '?0' }
+   
+   return requests.get(url, headers=headers).json()
+ 
+ 
+def parse_article_page(url):
+  soup = BeautifulSoup(requests.get(url).text, "html.parser")
+  result = {
+        "title": soup.select_one(".section-header-text").get_text(strip=True),
+        "date": soup.select_one(".articles-header-date .black").get_text(strip=True),
+        "time": soup.select_one(".articles-header-date .red").get_text(strip=True),
+        "text": soup.select_one("#mainContent").get_text("\n", strip=True),
+        "category": soup.find("meta", {"property": "og:url"})["content"].split("/")[5],
+        "url": url,
+        "is_video": False,
+        "image": None,
+        "video_poster": None,
+        "video_url": None
+    }
   
-    article_id = article.get("articleid")
-    title = article.get("title")
-    media_url = article.get("MediaUrl")
-    category = article.get("articletype")
-    url_path = article.get("Url")  # already a full path
-
-    full_url = f"https://www.mtv.com.lb{url_path}"
-
-    print("ID:", article_id)
-    print("Category:", category)
-    print("URL:", full_url)
-    print("Title:", title)
-    print("Image:", media_url)
-    print("-" * 60)
